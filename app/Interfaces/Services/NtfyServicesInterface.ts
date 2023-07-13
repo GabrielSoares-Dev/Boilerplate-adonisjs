1144e29c53721c4922d8ca9801fa0c9e6feb5217
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2023-07-12 21:37:03 -0300

    feat(fix interface): fix

--- app/Interfaces/Services/NtfyServicesInterface.ts
+++ app/Interfaces/Services/NtfyServicesInterface.ts
@@ -1,10 +1,4 @@
 import type { SendNotificationProps } from 'App/Services/types/Http/NtfyServices/SendNotificationProps'
 export interface NtfyServicesInterface {
-  sendNotification({
-    topic,
-    title = '',
-    message = '',
-    priority = 5,
-    actions,
-  }: SendNotificationProps)
+  sendNotification(body: SendNotificationProps)
 }
